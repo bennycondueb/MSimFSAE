5e06d9f7977b199c72fb2c4dcc4bf34aa13c663a
bennycondueb <b.benzi@protonmail.com>
2026-05-22 19:21:43 +0200

    fix: fixing flake8 errors

--- ros2_ws/src/system_bringup/launch/system_bringup_launch.py
+++ ros2_ws/src/system_bringup/launch/system_bringup_launch.py
@@ -1,32 +1,31 @@
 from ament_index_python.packages import get_package_share_directory, get_package_share_path
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution, Command
-from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch.substitutions import Command, PathJoinSubstitution
 from launch_ros.descriptions import ParameterValue
-
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     ros_gz_sim_pkg_path = get_package_share_directory('ros_gz_sim')
-    simulator_description_pkg_path = FindPackageShare('simulator_description')  # Replace with your own package name
+    sim_description_pkg_path = FindPackageShare('simulator_description')
     gz_launch_path = PathJoinSubstitution([ros_gz_sim_pkg_path, 'launch', 'gz_sim.launch.py'])
     path_to_urdf = get_package_share_path('simulator_description') / 'urdf' / 'racecar.xacro'
 
     return LaunchDescription([
-        #SetEnvironmentVariable(
-        #    'GZ_SIM_RESOURCE_PATH',
-        #    PathJoinSubstitution([example_pkg_path, 'models'])
-        #),
-        #SetEnvironmentVariable(
-        #    'GZ_SIM_PLUGIN_PATH',
-        #    PathJoinSubstitution([example_pkg_path, 'plugins'])
-        #),
+        # SetEnvironmentVariable(
+        #     'GZ_SIM_RESOURCE_PATH',
+        #     PathJoinSubstitution([example_pkg_path, 'models'])
+        # ),
+        # SetEnvironmentVariable(
+        #     'GZ_SIM_PLUGIN_PATH',
+        #     PathJoinSubstitution([example_pkg_path, 'plugins'])
+        # ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(gz_launch_path),
             launch_arguments={
-                'gz_args': PathJoinSubstitution([simulator_description_pkg_path, 'worlds/empty_custom_world.sdf']),  # Replace with your own world file
+                'gz_args': PathJoinSubstitution([sim_description_pkg_path, 'worlds/empty.sdf']),
                 'on_exit_shutdown': 'True'
             }.items(),
         ),
@@ -42,7 +41,7 @@ def generate_launch_description():
         # ),
 
 
-        # Node for the process xacro -> urdf -> gazebo 
+        # Node for the process xacro -> urdf -> gazebo
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -51,9 +50,9 @@ def generate_launch_description():
                     Command(['xacro ', str(path_to_urdf)]), value_type=str
                 )
             }]
-        ), 
+        ),
 
-        # Node for the process topic -> robot spawning 
+        # Node for the process topic -> robot spawning
         Node(
             package='ros_gz_sim',
             executable='create',
@@ -67,8 +66,8 @@ def generate_launch_description():
             output='screen'
         ),
 
-        
-        # Debug node just for our easyness 
+
+        # Debug node just for our easyness
         Node(
             package='path_planning',
             executable='path_planning',
--- ros2_ws/src/system_bringup/setup.py
+++ ros2_ws/src/system_bringup/setup.py
@@ -1,6 +1,7 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
-import os 
-from glob import glob 
 
 package_name = 'system_bringup'
 
@@ -12,7 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')) )
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     package_data={'': ['py.typed']},
     install_requires=['setuptools'],
